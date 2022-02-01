5bba5238e563e5c764acd94376893290b618998b
Mart Anthony Salazar <69393826+mart-anthony-stark@users.noreply.github.com>
2022-02-01 11:22:51 +0000

    added list exercise 2

--- ListExercises.hs
+++ ListExercises.hs
@@ -4,3 +4,10 @@
 elem :: (Eq a) => a -> [a] -> Bool
 elem _ [] = FALSE
 elem e (x:xs) = (e==x) || elem e xs
+
+--2. Create a function nub that removes all duplicates from a given list
+nub :: (Eq a) => [a] -> [a]
+nub [] = []
+nub (x:xs)
+  | x `elem` xs = nub xs
+  | otherwise = x:nub xs
