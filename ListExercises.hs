c318055ccce2eb798ecf4b2eb50f1a4025ea0f27
Mart Anthony Salazar <69393826+mart-anthony-stark@users.noreply.github.com>
2022-02-01 11:32:03 +0000

    added list exercise 4

--- ListExercises.hs
+++ ListExercises.hs
@@ -18,3 +18,13 @@ isAsc [] = True
 isAsc [x] = True
 isAsc (x:xy:xs) =
   (x<=y) && isAsc(y:xs)
+
+--4. Directed graph
+--Create a function hasPath that determines if a path from one node to another exists within a directed graph
+hasPath :: [(Int, Int)] -> Int -> Int -> Bool
+hasPath [] x y = x == y
+hasPath xs x y
+  | x==y = True
+  | otherwise =
+    let xs' = [(n,m) | (n,m) <- xs, n/=x] in
+    or [hasPath xs' m y | (n,m) <- xs, n==x]
